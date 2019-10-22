2578feac889f52d0f29df5b41e90c2a555b10c1b
nizar04 <33844522+nizar04@users.noreply.github.com>
2019-10-22 19:59:06 +0100

    teacherService

--- internspace-ejb/src/main/java/com/internspace/ejb/TeacherEJB.java
+++ internspace-ejb/src/main/java/com/internspace/ejb/TeacherEJB.java
@@ -17,34 +17,34 @@ public class TeacherEJB implements TeacherEJBLocal {
 	@PersistenceContext
 	EntityManager em;
 	
-
+@Override
 	public List<FYPFile> getPendingFYPFiles() {
 		
-		return em.createQuery("FROM " + FYPFile.class.getName()  + " f").getResultList();
+		return em.createQuery("FROM " + FYPFile.class.getName()  + " f where status=pending ").getResultList();
 
 	}
 
-	
+@Override
 	public boolean PrevalidateFYPFile(FYPFile file) {
 		return false;
 	}
 
-
+@Override
 	public List<FYPFile> getSupervisedFYPfiles() {
 	
 		return null;
 	}
 
-	
+@Override
 	public List<FYPFile> getprotractoredFYPfiles() {
 		return null;
 	}
 
-
+@Override
 	public boolean ValidateMajorModification(FYPFile f) {
 		return false;
 	}
-
+@Override
 	public void ProposeFYPCategory(FYPCategory F) {
 		System.out.println("adding category");
 em.persist(F);		
