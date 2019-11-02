bf38392b1bbfd754af37077d145f491a4eec4766
Rayene LIMEM <rayene.limem@esprit.tn>
2019-11-03 00:06:47 +0100

    accept modification

--- internspace-ejb/src/main/java/com/internspace/ejb/InternshipDirectorEJB.java
+++ internspace-ejb/src/main/java/com/internspace/ejb/InternshipDirectorEJB.java
@@ -32,6 +32,7 @@ import com.internspace.entities.fyp.FYPCategory;
 import com.internspace.entities.fyp.FYPFile;
 import com.internspace.entities.fyp.FYPFile.FYPFileStatus;
 import com.internspace.entities.fyp.FYPFileArchive;
+import com.internspace.entities.fyp.FYPFileModification;
 import com.internspace.entities.fyp.FYPIntervention;
 import com.internspace.entities.fyp.FYPIntervention.TeacherRole;
 import com.internspace.entities.fyp.FYPSubject;
@@ -573,7 +574,11 @@ public class InternshipDirectorEJB implements InternshipDirectorEJBLocal{
 		
 		em.persist(s);
 		em.flush();*/
-		servicemodif.getAllFilesModification();
+		
+		FYPFileModification f = em.find(FYPFileModification.class, id);
+		if(f.getIsChanged().equals(true)) {
+			f.setIsConfirmed(true);
+		}
 		
 	}
 
