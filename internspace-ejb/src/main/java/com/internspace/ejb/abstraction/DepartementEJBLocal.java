82c311976c82969f838cb726709798a3a498d609
Yassine Ben Hamida <yacinbenhamida@hotmail.fr>
2019-12-04 19:34:25 +0100

    updated Department Entity & CRUD

--- internspace-ejb/src/main/java/com/internspace/ejb/abstraction/DepartementEJBLocal.java
+++ internspace-ejb/src/main/java/com/internspace/ejb/abstraction/DepartementEJBLocal.java
@@ -5,8 +5,10 @@ import java.util.List;
 import com.internspace.entities.university.Departement;
 
 public interface DepartementEJBLocal {
-	int addDepartment(Departement departement);
+	long addDepartment(Departement departement);
 	public List<Departement> getAllDepartments();
+	public List<Departement> getAllDepartmentsOfSite(long siteId);
 	int updateDepartment(Departement departement);
 	int deleteDepartment(long id);
+	Departement getDeptById(long id);
 }
